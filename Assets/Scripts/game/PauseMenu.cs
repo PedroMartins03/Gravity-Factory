a7ce39d2ff03053b250c504b93c392579f154115
BigCucas <202817444+PedroMartinsIPS@users.noreply.github.com>
2026-07-20 20:09:26 +0100

    feito o menu da pausa durante o jogo

---
+++ Assets/Scripts/game/PauseMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
